39cbfac472151be8f0ffaaaa33ae11145022415e
emiemiemi <emijoaco_pereyra@hotmail.com>
2024-11-22 09:29:38 -0300

    Fixing build for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 import Welcomer from './components/welcomer'
 import Footer from './components/footer'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
