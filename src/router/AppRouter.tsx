ebbbe8a70b86de1601dd3dbe7f5f303ba5b2d784
emiemiemi <emijoaco_pereyra@hotmail.com>
2024-12-23 09:35:23 -0300

    added @ alias, title and icon

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -3,13 +3,12 @@ import Home from '../pages/Home'
 import NotFound from '../pages/NotFound'
 import About from '../pages/About'
 
-
-
 export const AppRouter = () => {
     return (
         <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/about" element={<About/>} />
+            <Route path="/writeups" element/>
             <Route path='*' element={<NotFound/>} />
         </Routes>
     )
